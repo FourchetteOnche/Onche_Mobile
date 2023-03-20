0da99210a08353801078901d7083917ece6e8978
FourchetteOnche <thomaspgo09@gmail.com>
2023-03-21 00:20:13 +0100

    Optimisation du code

--- app/src/main/java/com/fourchette/onchemobile/MainActivity.java
+++ app/src/main/java/com/fourchette/onchemobile/MainActivity.java
@@ -15,44 +15,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Déclaration de la WebView et du callback de chargement de fichier
     private WebView webView;
     private ValueCallback<Uri[]> filePathCallbackLegacy;
-
     private static final int REQUEST_CODE_FILE_PICKER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Utilisation du layout activity_main.xml pour définir l'interface utilisateur
         setContentView(R.layout.activity_main);
 
-        //Trouver l'élément WebView dans le layout
         webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                try {
+                    startActivityForResult(fileChooserParams.createIntent(), REQUEST_CODE_FILE_PICKER);
+                    filePathCallbackLegacy = filePathCallback;
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Sélecteur de fichier indisponible", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                return true;
+            }
+        });
 
-        // Activer JavaScript
-        webView.getSettings().setJavaScriptEnabled(true);
-
-        // Load the webpage with default cache mode
-        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setDomStorageEnabled(true);
         webView.loadUrl("https://onche.org/forum/1/blabla-general/");
 
-        // Activer les plugins
-        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
-
-        // Activer le chargement de contenu mixte (http et https)
-        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-
-        // Active le zoom/dézoom
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setDisplayZoomControls(false);
-
-        // Activer le stockage DOM (Document Object Model) pour la gestion des données locales
-        webView.getSettings().setDomStorageEnabled(true);
-
-        // Cache les pubs sur le site
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -65,25 +61,8 @@ public class MainActivity extends AppCompatActivity {
                         "})()");
             }
         });
-
-        // Autoriser les téléchargements de fichiers
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-                try {
-                    Intent intent = fileChooserParams.createIntent();
-                    startActivityForResult(intent, REQUEST_CODE_FILE_PICKER);
-                    filePathCallbackLegacy = filePathCallback;
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(MainActivity.this, "Sélecteur de fichier indisponible", Toast.LENGTH_SHORT).show();
-                    return false;
-                }
-                return true;
-            }
-        });
     }
 
-    // Revenir à la page précédente lorsque le bouton retour est pressé
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
@@ -93,17 +72,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Gérer les téléchargements de fichiers
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_FILE_PICKER && filePathCallbackLegacy != null) {
             Uri[] results = null;
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    String dataString = data.getDataString();
-                    if (dataString != null) {
-                        results = new Uri[]{Uri.parse(dataString)};
-                    }
+            if (resultCode == RESULT_OK && data != null) {
+                String dataString = data.getDataString();
+                if (dataString != null) {
+                    results = new Uri[]{Uri.parse(dataString)};
                 }
             }
             filePathCallbackLegacy.onReceiveValue(results);
